ad9552d0c5735b38c73153b09a8400f2223e35d7
mehejabinjannat <mehejabinjannat4@gmail.com>
2026-04-13 12:17:08 +0600

    Create BookAccess.java

---
+++ Module 1/Creating Constructor/BookAccess.java
@@ -0,0 +1,15 @@
+public class BookAccess {
+    public static void main(String s[]) {
+        Book book1 = new Book("Atomic Habits", "James Clear", 30.00f);
+
+        Book book2 = new Book();
+        book2.setTitle("Sapiens");
+        book2.setAuthor("Yuval Noah Harari");
+        book2.setPrice(25.00f);
+
+        System.out.println("The first book object is ");
+        System.out.println(book1);
+        System.out.println("The second book object is ");
+        System.out.println(book2);
+    }
+}
